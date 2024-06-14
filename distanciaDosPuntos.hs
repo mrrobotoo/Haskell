fbff1620b35bad83c58deb031b2b11a555a32760
Carlos Ruhal Sebastian Ruiz Perez <sebastianperezruiz6@gmail.com>
2024-06-13 18:49:22 -0600

    distancia entre dos puntos

---
+++ distanciaDosPuntos.hs
@@ -0,0 +1,10 @@
+-- laem.hs
+
+distanciaPuntos :: (Floating a) => (a, a) -> (a, a) -> a
+distanciaPuntos (x1, y1) (x2, y2) = sqrt ((x2 - x1)^2 + (y2 - y1)^2)
+
+
+--Aqui esta una calculadora para corroborar que jalara chido https://www.mathway.com/es/popular-problems/Finite%20Math/635667
+main :: IO ()
+main = do
+    print $ distanciaPuntos (-5, 4) (-3, 6)
