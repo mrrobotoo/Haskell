13afe3f84118cc645d5a53605a819932bed50c5c
Carlos Ruhal Sebastian Ruiz Perez <sebastianperezruiz6@gmail.com>
2024-06-13 18:41:48 -0600

    suma elementos impares

---
+++ sumaElementosImpares.hs
@@ -0,0 +1,9 @@
+-- laem.hs
+
+sumaElementosImpares :: [Int] -> Int
+sumaElementosImpares lista = sum [x | x <- lista, odd x]
+
+
+main :: IO ()
+main = do
+    print $ sumaElementosImpares [3, 5, 4, 8, 3, 2, 11, 10, 7, 6]
